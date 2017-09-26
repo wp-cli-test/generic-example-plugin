a63e2d053dc1d9363066a04111f90a9b0c0b3f7d
Alain Schlesser <alain.schlesser@gmail.com>
2017-09-26 08:03:22 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+# example-plugin
+
+This WordPress plugin is created for CI for the WP-CLI.
+Don't remove this plugin!!
---
+++ example-plugin.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     Example Plugin
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     example-plugin
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Example_Plugin
+ */
